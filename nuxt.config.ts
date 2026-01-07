3bc56cc94d1318fd0d7a9a74bfa7c03fb80f60e4
ienming <hsieh.alan.0726@gmail.com>
2026-01-07 22:31:01 +0800

    chore: add config for SPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,10 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
     port: 3000,
   },
+  ssr: false,
+  app: {
+    baseURL: 'shyline',
+  },
   css: [
     'vue-color/style.css',
     '~/assets/css/main.css',
